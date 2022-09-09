0e905213d7efc0e2920343cc858a545e6f47719f
Cengiz TORU <cengiztoru@gmail.com>
2022-09-09 22:00:27 +0300

    Prepare some constants

--- app/src/main/java/com/muhammad/cengiztoru/dynamicisland/ui/components/Notch.kt
+++ app/src/main/java/com/muhammad/cengiztoru/dynamicisland/ui/components/Notch.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.muhammad.cengiztoru.dynamicisland.ui.util.Constant
 
 
 /**
@@ -21,16 +18,15 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun Notch() {
-    val configuration = LocalConfiguration.current
-    val islandWidth = (configuration.screenWidthDp / 2.5f).dp
-    val islandHeight = (configuration.screenHeightDp / 18f).dp
+    val notchWidth = Constant.getMinIslandWidth()
+    val notchHeight = Constant.getMinIslandHeight()
 
     Card(
         modifier = Modifier
-            .width(islandWidth)
-            .height(islandHeight),
-        backgroundColor = Color.Black,
-        shape = RoundedCornerShape(32.dp),
+            .width(notchWidth)
+            .height(notchHeight),
+        backgroundColor = Constant.dynamicIslandBackgroundColor,
+        shape = Constant.dynamicIslandBackgroundShape,
     ) {
 
         Row(
---
+++ app/src/main/java/com/muhammad/cengiztoru/dynamicisland/ui/util/Constant.kt
@@ -0,0 +1,32 @@
+package com.muhammad.cengiztoru.dynamicisland.ui.util
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * Created by Cengiz TORU on 09/09/2022.
+ * cengiztoru@gmail.com
+ */
+object Constant {
+
+    val dynamicIslandBackgroundShape = RoundedCornerShape(32.dp)
+    val dynamicIslandBackgroundColor = Color.Black
+
+    @Composable
+    fun getMinIslandWidth(): Dp {
+        val configuration = LocalConfiguration.current
+        return (configuration.screenWidthDp / 2.5f).dp
+    }
+
+    @Composable
+    fun getMinIslandHeight(): Dp {
+        val configuration = LocalConfiguration.current
+        return (configuration.screenHeightDp / 18f).dp
+    }
+
+}
